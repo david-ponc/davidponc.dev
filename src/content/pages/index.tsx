5f82dc4eb0f88fe0be3c6ce525df04a42f9976a5
David Ponce Vargas <buap.david@gmail.com>
2022-04-14 20:57:43 -0500

    chore: update content of projects section

--- src/content/pages/index.tsx
+++ src/content/pages/index.tsx
@@ -4,60 +4,59 @@ const mainProjectsInfo = [
 		description:
 			'Esta aplicación fue inspirada por el grupo Dona con el lema de hacer una lista de tareas mas humana. La aplicación cuenta con un sitema de usuarios autenticados mediante correo electronico y contraseña.',
 		tags: ['React', 'TypeScript', 'Next.js', 'Styled Components'],
-		images: [
-			'/projects/images/todo-app-light.png',
-			'/projects/images/todo-app-dark.png',
-		],
-		links: [
-			'https://todo.davidponc.dev',
-			'https://github.com/david-ponc/todo-app-client',
-		],
+		image: {
+			light: '/projects/images/todo-app-light.png',
+			dark: '/projects/images/todo-app-dark.png',
+		},
+		url: {
+			deployment: 'https://todo.davidponc.dev',
+			repository: 'https://github.com/david-ponc/todo-app-client',
+		},
 	},
 	{
 		title: 'Concurso Verano Frontend',
 		description:
 			'Sitio web estatico para el concurso de verano frontend organizado por la empresa codigofacilito. Cuenta con diversas secciones que cuentan la historia de Cody, asi como una galeria de fotos de los lugares donde estuvo.',
 		tags: ['HTML', 'CSS', 'JavaScript'],
-		images: [
-			'/projects/images/verano-frontend.png',
-			'/projects/images/verano-frontend.png',
-		],
-		links: [
-			'https://david-ponc.github.io/vacaciones-cody/',
-			'https://github.com/david-ponc/vacaciones-cody',
-		],
+		image: {
+			light: '/projects/images/verano-frontend.png',
+		},
+		url: {
+			deployment: 'https://david-ponc.github.io/vacaciones-cody/',
+			repository: 'https://github.com/david-ponc/vacaciones-cody',
+		},
 	},
 ];
 
 const extraProjectsInfo = [
 	{
-		title: 'covid-19-countries-list',
+		name: 'covid-19-countries-list',
 		description: 'Aplicación que muestra información por pais del covid-19.',
-		tags: ['React.js', 'JavaScript', 'CSS'],
-		links: [
-			'https://covid.davidponc.dev/',
-			'https://github.com/david-ponc/covid-19-countries-list',
-		],
+		url: {
+			deployment: 'https://covid.davidponc.dev/',
+			repository: 'https://github.com/david-ponc/covid-19-countries-list',
+		},
+		languages: ['JavaScript', 'CSS', 'HTML'],
 	},
 	{
-		title: 'upload-file',
+		name: 'upload-file',
 		description:
 			'Aplicación que permite la subida de imagenes utilizando firebase.',
-		tags: ['Next.js', 'JavaScript', 'CSS', 'Firebase'],
-		links: [
-			'https://upload-file.vercel.app/',
-			'https://github.com/david-ponc/upload-file',
-		],
+		url: {
+			deployment: 'https://upload-file.vercel.app/',
+			repository: 'https://github.com/david-ponc/upload-file',
+		},
+		languages: ['JavaScript', 'CSS'],
 	},
 	{
-		title: 'medio-ambiente-fcc',
+		name: 'medio-ambiente-fcc',
 		description:
 			'Sitio web para difusion de eventos relacionados con el medio ambiente.',
-		tags: ['HTML', 'JavaScript', 'CSS'],
-		links: [
-			'https://david-ponc.github.io/medio-ambiente-fcc/',
-			'https://github.com/david-ponc/medio-ambiente-fcc',
-		],
+		url: {
+			deployment: 'https://david-ponc.github.io/medio-ambiente-fcc/',
+			repository: 'https://github.com/david-ponc/medio-ambiente-fcc',
+		},
+		languages: ['HTML', 'CSS', 'JavaScript'],
 	},
 ];
 
