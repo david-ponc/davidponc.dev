f586fd767dae8017f8e3ad50d921fcf511c92fc3
David Ponce Vargas <32694631+david-ponc@users.noreply.github.com>
2022-04-22 12:03:04 -0500

    fix: add definition children prop

--- src/components/draft-lines-wrap/draft-lines-wrap.tsx
+++ src/components/draft-lines-wrap/draft-lines-wrap.tsx
@@ -1,10 +1,13 @@
 import clsx from 'clsx';
-import { FC } from 'react';
+import { FC, ReactNode } from 'react';
 
 import styles from './draft-lines-wrap.module.css';
 
-export const DraftLinesWrap: FC = ({ children }) => (
-	<div className={styles.wrapper}>
+export const DraftLinesWrap: FC<{ children?: ReactNode }> = ({
+	children,
+	...props
+}) => (
+	<div className={styles.wrapper} {...props}>
 		<div className={clsx(styles.line, styles.top)} />
 		<div className={clsx(styles.line, styles.bottom)} />
 		<div className={clsx(styles.line, styles.left)} />
