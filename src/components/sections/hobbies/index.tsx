0e7bc9e8ceb65f6ca5817e82f050ad24c70981ea
David Ponce Vargas <32694631+david-ponc@users.noreply.github.com>
2022-04-25 09:44:18 -0500

    feat: add hobbies section

--- src/components/sections/hobbies/index.tsx
+++ src/components/sections/hobbies/index.tsx
@@ -1,4 +1,8 @@
+import Image from 'next/image';
 import { forwardRef } from 'react';
+
+import { Link } from '~/components/primitives';
+import { hobbiesContent } from '~/content/pages/hobbies';
 
 import styles from './hobbies.module.css';
 
@@ -6,7 +10,36 @@ import styles from './hobbies.module.css';
 export const HobbiesSection = forwardRef<HTMLDivElement, {}>((props, ref) => {
 	return (
 		<section ref={ref} id='ocupaciones' className={styles.container} {...props}>
-			<h2 className={styles.title}>Ocupaciones</h2>
+			<div className={styles.mask}>
+				<div className={styles.pattern} />
+			</div>
+			<figure>
+				<Link
+					href={hobbiesContent.image.href}
+					passHref
+					className={styles.imageWrapper}
+				>
+					<Image
+						src={hobbiesContent.image.cover}
+						width={1200}
+						height={514.29}
+						quality={100}
+						alt={hobbiesContent.image.alt}
+						objectFit='contain'
+						objectPosition='center'
+						placeholder='blur'
+						blurDataURL={hobbiesContent.image.blurData}
+					/>
+				</Link>
+			</figure>
+			<article className={styles.content}>
+				<h2 className={styles.title}>{hobbiesContent.title}</h2>
+				{hobbiesContent.paragraphs.map((paragraph, index) => (
+					<p key={index} className={styles.text}>
+						{paragraph}
+					</p>
+				))}
+			</article>
 		</section>
 	);
 });
