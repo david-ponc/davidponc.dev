865e96e706f98d6316ac1086f9bc60a130ac7942
David Ponce Vargas <buap.david@gmail.com>
2023-08-07 18:37:47 -0600

    style: apply lint & format rules

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,20 @@
 import '~/styles/globals.css';
 
+import Loglib from '@loglib/tracker/react';
 import type { AppProps } from 'next/app';
 
 import { ColorModeContextDynamicProvider } from '~/contexts/color-mode-context';
-import Loglib from "@loglib/tracker/react"
 
 function App({ Component, pageProps }: AppProps) {
 	return (
 		<ColorModeContextDynamicProvider>
 			<Component {...pageProps} />
-			<Loglib config={{
-        id: "davidponc",
-        host: "https://www.loglib.io"
-      }} />
+			<Loglib
+				config={{
+					id: 'davidponc',
+					host: 'https://www.loglib.io',
+				}}
+			/>
 		</ColorModeContextDynamicProvider>
 	);
 }
