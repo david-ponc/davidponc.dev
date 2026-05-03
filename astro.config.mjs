70e754ce6e4baf06cbaa01c1020ecf6afbf73137
David Ponce Vargas <buap.david@gmail.com>
2026-05-02 19:48:42 -0600

    config: add Vercel adapter, i18n routing, and update tsconfig

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,27 +4,41 @@ import tailwindcss from '@tailwindcss/vite';
 import { defineConfig, fontProviders } from 'astro/config';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
-	vite: {
-		plugins: [tsconfigPaths(), tailwindcss()],
+  vite: {
+      plugins: [tsconfigPaths(), tailwindcss()],
 	},
-	fonts: [
-		{
-			provider: fontProviders.fontsource(),
-			name: 'Geist',
-			cssVariable: '--font-app-sans',
-			weights: ['100 900'],
-			styles: ['normal'],
-			fallbacks: ['sans-serif'],
-		},
-		{
-			provider: fontProviders.fontsource(),
-			name: 'Geist Mono',
-			cssVariable: '--font-app-mono',
-			weights: ['100 900'],
-			styles: ['normal'],
-			fallbacks: ['monospace'],
-		},
+
+  fonts: [
+      {
+          provider: fontProviders.fontsource(),
+          name: 'Geist',
+          cssVariable: '--font-app-sans',
+          weights: ['100 900'],
+          styles: ['normal'],
+          fallbacks: ['sans-serif'],
+      },
+      {
+          provider: fontProviders.fontsource(),
+          name: 'Geist Mono',
+          cssVariable: '--font-app-mono',
+          weights: ['100 900'],
+          styles: ['normal'],
+          fallbacks: ['monospace'],
+      },
 	],
+
+  output: "server",
+  adapter: vercel(),
+
+  i18n: {
+    defaultLocale: "es",
+    locales: ["es", "en"],
+    routing: {
+      prefixDefaultLocale: false,
+    },
+  },
 });
