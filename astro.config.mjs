45ccbb58bad913b799a7726bf8a620438c94ce8a
David Ponce Vargas <buap.david@gmail.com>
2026-05-18 12:53:53 -0600

    config(astro): normalize formatting in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,48 +1,47 @@
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
+import vercel from '@astrojs/vercel';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig, fontProviders } from 'astro/config';
 import tsconfigPaths from 'vite-tsconfig-paths';
-
-import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 // @ts-check
 
 export default defineConfig({
-  site: 'https://davidponc.dev',
-  integrations: [react(), sitemap()],
-  vite: {
-      plugins: [tsconfigPaths(), tailwindcss()],
+	site: 'https://davidponc.dev',
+	integrations: [react(), sitemap()],
+	vite: {
+		plugins: [tsconfigPaths(), tailwindcss()],
 	},
 
-  fonts: [
-      {
-          provider: fontProviders.fontsource(),
-          name: 'Geist',
-          cssVariable: '--font-app-sans',
-          weights: ['100 900'],
-          styles: ['normal'],
-          fallbacks: ['sans-serif'],
-      },
-      {
-          provider: fontProviders.fontsource(),
-          name: 'Geist Mono',
-          cssVariable: '--font-app-mono',
-          weights: ['100 900'],
-          styles: ['normal'],
-          fallbacks: ['monospace'],
-      },
+	fonts: [
+		{
+			provider: fontProviders.fontsource(),
+			name: 'Geist',
+			cssVariable: '--font-app-sans',
+			weights: ['100 900'],
+			styles: ['normal'],
+			fallbacks: ['sans-serif'],
+		},
+		{
+			provider: fontProviders.fontsource(),
+			name: 'Geist Mono',
+			cssVariable: '--font-app-mono',
+			weights: ['100 900'],
+			styles: ['normal'],
+			fallbacks: ['monospace'],
+		},
 	],
 
-  output: "server",
-  adapter: vercel(),
+	output: 'server',
+	adapter: vercel(),
 
-  i18n: {
-    defaultLocale: "es",
-    locales: ["es", "en"],
-    routing: {
-      prefixDefaultLocale: false,
-    },
-  },
+	i18n: {
+		defaultLocale: 'es',
+		locales: ['es', 'en'],
+		routing: {
+			prefixDefaultLocale: false,
+		},
+	},
 });
